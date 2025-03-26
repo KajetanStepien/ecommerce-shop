5630d29dcebc10136aa39769f427517c5061ada5
Kajetan Stępień <kajetanstepien@MacBook-Air-Kajetan.local>
2025-03-26 13:23:54 +0100

    more work on products list

--- front-end/src/components/ExpandableMenu/ExpandableMenu.jsx
+++ front-end/src/components/ExpandableMenu/ExpandableMenu.jsx
@@ -3,6 +3,7 @@ import ARROW_UP from "../../assets/arrow-up.svg";
 import styles from "./ExpandableMenu.module.css";
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { CATEGORIES } from "../../constants/categories";
 
 export function ExpandableMenu() {
   const [isOpen, setIsOpen] = useState([false, false, false, false]);
@@ -22,40 +23,32 @@ export function ExpandableMenu() {
   return (
     <div className={styles.expandableMenu}>
       <span>Kobieta</span>
-      <button onClick={() => handleExpand(0)}>
-        Odzież <img src={iconSelector(0)} />
-      </button>
-      {isOpen[0] && (
-        <div className={styles.additionalOptions}>
-          <NavLink>
-            <button>Koszulki</button>
-          </NavLink>
-          <NavLink>
-            <button>Swetry</button>
-          </NavLink>
-          <NavLink>
-            <button>Spodnie</button>
-          </NavLink>
-        </div>
-      )}
-      <button onClick={() => handleExpand(1)}>
-        Obuwie <img src={iconSelector(1)} />
-      </button>
-      {isOpen[1] && (
-        <div className={styles.additionalOptions}>
-          <NavLink>
-            <button>Eleganckie</button>
-          </NavLink>
-        </div>
-      )}
-      <button onClick={() => handleExpand(2)}>
-        Akcesoria <img src={iconSelector(2)} />
-      </button>
-      {isOpen[2] && <div className={styles.additionalOptions}></div>}
-      <button onClick={() => handleExpand(3)}>
-        Sport <img src={iconSelector(3)} />
-      </button>
-      {isOpen[3] && <div className={styles.additionalOptions}></div>}
+      {CATEGORIES.map((category, index) => {
+        return (
+          <>
+            <button
+              onClick={() => handleExpand(index)}
+              key={category.categoryName}
+            >
+              {category.categoryName} <img src={iconSelector(index)} alt="" />
+            </button>
+            {isOpen[index] && (
+              <div className={styles.additionalOptions}>
+                {category.subCategories.map((subCategory) => {
+                  return (
+                    <NavLink
+                      to={`${category.path}/${subCategory.path}`}
+                      key={subCategory.categoryName}
+                    >
+                      {subCategory.categoryName}
+                    </NavLink>
+                  );
+                })}
+              </div>
+            )}
+          </>
+        );
+      })}
     </div>
   );
 }
